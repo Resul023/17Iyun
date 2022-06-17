a6234a45cbcd8f740c339ed5f20cb0689d0304dc
Resul023 <73m6u7h@code.edu.az>
2022-06-17 03:23:43 -0700

    HomeWork

---
+++ Pustok/Pustok/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Pustok.Areas.Manage.ViewModel;
+using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+        /*public async Task<IActionResult> CreateAdmin()
+        {
+            AppUser admin = new AppUser
+            {
+                FullName = "Super Admin",
+                UserName = "SPAdmin",
+
+            };
+            var result = await _userManager.CreateAsync(admin, "Hello123");
+            if (!result.Succeeded)
+            {
+                return Ok(result.Errors);
+            }
+            return View();
+        }*/
+        public IActionResult Login() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(AdminLoginViewModel admin) 
+        {
+            return Ok(admin);
+        
+        }
+    }
+}
---
+++ Pustok/Pustok/Areas/Manage/Controllers/SettingController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.DAL;
+using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SettingController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SettingController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var setting = _context.Settings.ToDictionary(x => x.Key, y => y.Value);
+            
+            return View(setting);
+        }
+        public IActionResult Edit(string key) 
+        {
+            var setting = _context.Settings.FirstOrDefault(x => x.Key == key);
+            if (setting==null)
+            {
+                return NotFound();
+            }
+           
+            return View(setting);
+        
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Setting setting) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var isExists = _context.Settings.FirstOrDefault(x => x.Key == setting.Key && x.Value == setting.Value);
+            if (isExists == null)
+            {
+                return NotFound();
+            }
+            isExists.Value = setting.Value;
+            isExists.Key = setting.Key;
+            _context.SaveChanges();
+            return RedirectToAction("index");
+
+        }
+    }
+}
